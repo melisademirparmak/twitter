5b13fd65c8d3a24c964f4c5a0e0b8e731a5d5580
melisademirparmak <melisademirparmaak@gmail.com>
2021-11-07 20:39:16 +0300

    React icons install

---
+++ src/components/SideBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AiOutlineTwitter } from '@react-icons/all-files/ai/AiOutlineTwitter';
+
+const SideBar = () => {
+  return (
+    <div>
+      <AiOutlineTwitter />
+    </div>
+  );
+};
+
+export default SideBar;
