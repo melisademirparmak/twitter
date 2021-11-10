529338eb2f280d6727089fc6b989e3c826325c52
melisademirparmak <melisademirparmaak@gmail.com>
2021-11-10 22:29:36 +0300

    Tweetbox finish

--- src/components/TweetBox.js
+++ src/components/TweetBox.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 function TweetBox() {
-    return (
-        <div>
-            
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <picture>
+            <img
+              src="https://www.mothermary.rw/images/img/lady.jpg"
+              alt="Avatar"
+            />
+          </picture>
+          <input type="text" placeholder="What's happening ?" />
         </div>
-    )
+
+        <button className="tweetBox__btn">Tweet</button>
+      </form>
+    </div>
+  );
 }
 
-export default TweetBox
+export default TweetBox;
