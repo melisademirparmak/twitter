1657a13098d73f8fa9d2768dc0ef27d14874c777
melisademirparmak <melisademirparmaak@gmail.com>
2021-11-11 20:16:52 +0300

    Post json and style create

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,44 @@
 import React from 'react';
+import { BsFillReplyFill } from '@react-icons/all-files/bs/BsFillReplyFill';
+import { AiOutlineRetweet } from '@react-icons/all-files/ai/AiOutlineRetweet';
+import { BiHeart } from '@react-icons/all-files/bi/BiHeart';
+import { FiShare } from '@react-icons/all-files/fi/FiShare';
 import Posts from '../api/post.json';
 
 function Post() {
   return (
-    <div classNmae="post">
+    <>
       {Posts.map((post) => (
         <div key={post.id}>
-          <div className="d-flex">
-            <img src={post.avatar} className="post__avatar" alt="Avatar" />
-            <h4 className="post__name">{post.name}</h4>
-            <h5 className="post__userName">{post.userName}</h5>
+          <div className="post">
+            <div className="post__avatar">
+              <img src={post.avatar} alt="" />
+            </div>
+            <div className="post__body">
+              <div className="post__header">
+                <div className="post__headerText">
+                  <h3>
+                    {post.name}
+                    <span className="post__headerSpecial">{post.userName}</span>
+                    <span>{post.time}</span>
+                  </h3>
+                </div>
+                <div className="post__headerDescription">
+                  <p>{post.text}</p>
+                </div>
+              </div>
+              <img src={post.photo} alt="" />
+              <div className="post__footer">
+                <BsFillReplyFill />
+                <AiOutlineRetweet />
+                <BiHeart />
+                <FiShare />
+              </div>
+            </div>
           </div>
-
-          <div className="post__text">{post.text}</div>
-          <img src={post.photo} className="post__photo" alt="PostPhoto" />
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
