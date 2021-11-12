7417e532924a19123f979c33934b36b4781030c5
melisademirparmak <melisademirparmaak@gmail.com>
2021-11-12 18:09:46 +0300

    Npm package react twitter embed

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton,
+} from 'react-twitter-embed';
 
 function Widgets() {
   return (
