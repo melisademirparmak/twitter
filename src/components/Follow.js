604802bf08bf01e35af64b766087b39930e1d2cd
melisademirparmak <melisademirparmaak@gmail.com>
2021-11-14 14:04:19 +0300

    Fix sass and follow structure create

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function Follow() {
-  return <div>follow</div>;
+  return (
+    <>
+      <div className="widgest">
+        <div className="widgest__container">
+          <div className="widgest__title">
+            <h2>Who to follow</h2>
+          </div>
+          <div className="follow">
+            <div className="follow__avatar">
+              <img
+                src="https://p.kindpng.com/picc/s/78-786678_avatar-hd-png-download.png"
+                alt=""
+              />
+            </div>
+            <div className="follow__user">
+              <span>Adam</span>
+              <span>@adam</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Follow;
